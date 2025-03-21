8a08ff7fbe5e63604d53bbd1bfc206835c3a249b
Vaisakh P V <vaisakhpv033@gmail.com>
2025-03-21 12:03:14 +0530

    fixed comment

--- 5-Sum-of-two-integers/Solution.java
+++ 5-Sum-of-two-integers/Solution.java
@@ -1,10 +1,10 @@
-# link: https://leetcode.com/problems/sum-of-two-integers/description/
+// link: https://leetcode.com/problems/sum-of-two-integers/description/
 class Solution {
     public static void main(String[] args) {
         Solution sol = new Solution();
         System.out.println(sol.getSum(1, 2));
     }
-    
+
     public int getSum(int a, int b) {
         while (b != 0){
             int temp = (a&b) << 1;
