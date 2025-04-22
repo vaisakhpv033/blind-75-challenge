c497a8aefaadc2e10388b8d1a312c51a1808e7f8
Vaisakh P V <vaisakhpv033@gmail.com>
2025-04-22 09:37:05 +0530

    Completed Day-37

---
+++ 37-Merge-Intervals/solution.py
@@ -0,0 +1,21 @@
+# link: https://leetcode.com/problems/merge-intervals/description/
+from typing import List
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals = sorted(intervals)
+
+        res = [] 
+        new_interval = intervals[0]
+        for idx in range(1, len(intervals)):
+            if new_interval[1] < intervals[idx][0]:
+                res.append(new_interval)
+                new_interval = intervals[idx]
+            else:
+                new_interval = [min(new_interval[0], intervals[idx][0]), max(new_interval[1], intervals[idx][1])]
+
+        res.append(new_interval)
+        return res 
+    
+
+# time complexity: O(NlogN)
+# space complexity: O(N)
