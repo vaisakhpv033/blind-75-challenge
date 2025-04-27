f16acc1f20f6d163324f98ac5acb05ee2e294f34
Vaisakh P V <vaisakhpv033@gmail.com>
2025-04-27 11:02:58 +0530

    Completed Day-42

---
+++ 42-Linked-List-Cycle/solution.py
@@ -0,0 +1,15 @@
+# link: https://leetcode.com/problems/linked-list-cycle/
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+            if slow == fast:
+                return True
+        return False
